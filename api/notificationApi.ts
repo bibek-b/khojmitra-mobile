f436ab5570130a7d0815c93683be470a5c503aad
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-05 12:12:12 +0545

    minor change

--- api/notificationApi.ts
+++ api/notificationApi.ts
@@ -2,7 +2,7 @@ import { NotificationPropType } from "@/types/notification";
 import api from "./axios";
 
 export const notificationApi = {
-    getNotifications : () => api.get('/notifications'),
+    getMyNotifications : (id: string) => api.get(`/notifications/${id}`),
     pushNotifications: (data: NotificationPropType) => api.post("/notifications", data),
     updateSeen : (id: string) => api.post(`/notifications/${id}/read`)
 }

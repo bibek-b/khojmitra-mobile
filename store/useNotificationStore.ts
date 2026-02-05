bafd3caca1d5109ab8b1fbd8e5bc54e3f2eda28f
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-05 12:12:56 +0545

    removed newNotification state

--- store/useNotificationStore.ts
+++ store/useNotificationStore.ts
@@ -1,10 +1,11 @@
-import { NotificationStoreType, ReceiveNotificationPropType } from "@/types/notification";
+import {
+  NotificationStoreType,
+  ReceiveNotificationPropType,
+} from "@/types/notification";
 import { create } from "zustand";
 
 export const useNotificationStore = create<NotificationStoreType>((set) => ({
-    newNotification: false,
-    setNewNotification: (value: boolean) => set({newNotification: value}),
-    notifications: [],
-    setNotifications: (data: ReceiveNotificationPropType[]) => set({notifications: data}),
-    addNotification: (newNotifi: ReceiveNotificationPropType) => set(state => ({notifications: [...state.notifications, newNotifi]}))
-}))
+  notifications: [],
+  setNotifications: (data: ReceiveNotificationPropType[]) =>
+    set({ notifications: data }),
+}));

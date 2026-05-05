61c319b16417e826312c31cfde1a1322c8fa5e8d
Bibek Gadel <vieekgadel49@gmail.com>
2026-05-05 13:45:08 +0545

    getNotificationById route added

--- api/notificationApi.ts
+++ api/notificationApi.ts
@@ -4,6 +4,7 @@ import api from "./axios";
 export const notificationApi = {
     getMyNotifications : (id: string) => api.get(`/notifications/${id}`),
     pushNotifications: (data: NotificationPropType) => api.post("/notifications", data),
+    getNotificationById : (id: string) => api.get(`/notifications/notification/${id}`),
     updateSeen : (id: string) => api.post(`/notifications/${id}/read`),
     sendPushToken : (userId: string, token: string) => api.post('/notifications/save-token', {userId, token})
 }

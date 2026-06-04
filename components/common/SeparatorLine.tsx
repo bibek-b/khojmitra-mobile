3bbf021bd44b8dbda50c8008150b7b39c5b95e75
Bibek Gadel <vieekgadel49@gmail.com>
2026-06-04 15:46:32 +0545

    separator line height reduced

--- components/common/SeparatorLine.tsx
+++ components/common/SeparatorLine.tsx
@@ -6,7 +6,7 @@ export default function SeparatorLine () {
     const { isDarkMode } = useContext(ThemeContext);
     return (
           <View
-          className={`h-[3px] w-full  ${isDarkMode ? "bg-white/30": "bg-black/30"}`}
+          className={`h-[1px] w-full  ${isDarkMode ? "bg-white/30": "bg-black/30"}`}
         />
     )
 }

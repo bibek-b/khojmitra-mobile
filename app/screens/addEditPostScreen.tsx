96a33dfee1d1284474bfc08b8e87434303659e8f
Bibek Gadel <vieekgadel49@gmail.com>
2026-06-16 14:01:37 +0545

    minor text bug fixed

--- app/screens/addEditPostScreen.tsx
+++ app/screens/addEditPostScreen.tsx
@@ -3,15 +3,16 @@ import { usePostStore } from "@/store/usePostStore";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useLayoutEffect } from "react";
 
-export default function AddReportScreen() {
+export default function AddEditPostScreen() {
   const params = useLocalSearchParams();
   const { isEditPost } = usePostStore();
 
   const idToUpdate = typeof params.idToUpdate === "object" ? params.idToUpdate?.[0]: params.idToUpdate;
   const navigation = useNavigation();
+  
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: isEditPost  ? "Edit Report" : "Add Report",
+      title: isEditPost  ? "Edit Post" : "Add Post",
     });
   }, [navigation]);
   return (
